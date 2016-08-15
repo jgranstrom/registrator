ed0eb0d9c32db6d343c6ea8e47a4dfcfebcddce5
John Granström <granstrom.john@gmail.com>
2016-08-15 14:14:47 +0200

    Fix etcd breaking version

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -9,7 +9,7 @@ import (
 	"regexp"
 	"strconv"
 
-	etcd2 "github.com/coreos/go-etcd/etcd"
+	etcd2 "gopkg.in/coreos/go-etcd.v2/etcd"
 	"github.com/gliderlabs/registrator/bridge"
 	etcd "gopkg.in/coreos/go-etcd.v0/etcd"
 )
